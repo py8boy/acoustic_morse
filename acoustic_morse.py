8f5d8e3c652427a6408411b0f2409371401b99ae
py8boy <theodorotheodore@gmail.com>
2020-07-19 21:06:04 -0300

    Criei o mecanismo que identifica, coverte e chama as funções para o programa rodar

--- acoustic_morse.py
+++ acoustic_morse.py
@@ -16,3 +16,61 @@ def dah():                                         # sub-rotina para emitir os s
 
 def breaks():                                       # 'regulamenta' os intervalos entre as palavras
     sleep(5)
+
+
+
+def verifyMsg(msg):                                             # adiciona a mensagem convertida na lista convertData
+    for char in msg:
+        convertedData.append(general[char])
+
+
+def soundMsg(msg_list):                                 # emite os sons respectivos ao sinais (. e -)
+    for char in msg_list:
+        print(char)
+        sleep(1)
+        if (char == ' '):
+            breaks()
+        for element in char:
+            if (element == '.'):
+                dit()
+            elif (element == '-'):
+                dah()
+
+
+def cleaningData(dataList):
+    dataList.clear()
+
+
+def callFunctions():
+    verifyMsg(userMsg)
+    soundMsg(convertedData)
+    cleaningData(convertedData)
+
+
+
+general = {'a': '.-', 'b': '-...',
+           'c': '-.-.', 'd': '-..',
+           'e': '.', 'f': '..-.',
+           'g': '--.', 'h': '....',
+           'i': '..', 'j': '.---',
+           'k': '-.-', 'l': '.-..',
+           'm': '--', 'n': '-.',
+           'o': '---', 'p': '.--.',
+           'q': '--.-', 'r': '.-.',
+           's': '...', 't': '-',
+           'u': '..-', 'v': '...-',
+           'w': '.--', 'x': '-..-',
+           'y': '-.--', 'z': '--..',
+           '1': '.----', '2': '..---',
+           '3': '...--', '4': '....-',
+           '5': '.....', '6': '-....',
+           '7': '--...', '8': '---..',
+           '9': '----.', '10': '-----', 
+           ' ': ' ', '.': '.-.-.-',
+           ',': '--..--', '?': '..--..'}
+
+convertedData = []
+
+print('\n \033[32m Do not type accents, exclamation marks or symbols \033[m')
+userMsg = input(str('Enter your message: ')).lower()
+callFunctions()
